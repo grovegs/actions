9a0ed8d9248b3b3ca2f90ab844730be333d984d6
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-11-09 20:44:03 +0300

    feat: add setup godot

---
+++ src/setup-godot/download_godot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <platform> <version> <path>"
+    exit 1
+fi
+
+platform="$1"
+version="$2"
+path="$3"
+
+case "$platform" in
+"linux")
+    platform="linux_x86_64"
+    ;;
+"macos")
+    platform="macos.universal"
+    ;;
+"windows")
+    platform="win64"
+    ;;
+*)
+    echo "Error: Unsupported platform '$platform'."
+    exit 1
+    ;;
+esac
+
+url="https://github.com/godotengine/godot/releases/download/${version}-stable/Godot_v${version}-stable_mono_${platform}.zip"
+
+if ! curl -L -o "$path/$(basename "$url")" "$url"; then
+    echo "Download failed."
+    exit 1
+fi
+
+file_name=$(basename "$url")
+echo "::set-output name=file::$path/$file_name"
---
+++ src/setup-godot/install_godot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <installation_file>"
+    exit 1
+fi
+
+installation_file="$1"
+
+if [ ! -f "$installation_file" ]; then
+    echo "Error: File '$installation_file' not found!"
+    exit 1
+fi
+
+file_dir=$(dirname "$installation_file")
+file_name=$(basename "$installation_file")
+cd "$file_dir" || exit 1
+
+if ! unzip "$file_name"; then
+    echo "Extraction failed."
+    exit 1
+fi
+
+rm "$file_name"
+
+echo "::set-output name=file::$file_dir/$file_name .zip)"
