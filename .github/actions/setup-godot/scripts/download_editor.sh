896f7ecd6fc16b4ad9e84de8a4572d3bb08f79b6
Onur Tanrıkulu <onurtnrkl@users.noreply.github.com>
2024-11-15 20:01:44 +0300

    feat: add export templates (#10)
    * refactor: rename setup godot scripts
    
    * feat: add download templates script
    
    * feat: add export templates
    
    * fix: read_version.sh
    
    * chore: rename read version script
    
    * fix: enable cross os archive
    
    * test: test macos only
    
    * refactor: simplify Godot setup workflow by consolidating cache logic
    
    * Update install_editor.sh
    
    * Update action.yml
    
    * test: update tests
    
    * Update action.yml
    
    * Update action.yml
    
    * Update setup-godot-tests.yml
    
    * Update action.yml
    
    * Update setup-godot-tests.yml
    
    * feat: add template platforms
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update download_templates.sh
    
    * Update action.yml
    
    * Update action.yml
    
    * Update setup-godot-tests.yml
    
    * Update action.yml
    
    * fix: update templates cache name

---
+++ .github/actions/setup-godot/scripts/download_editor.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <version> <runner_os>"
+    exit 1
+fi
+
+version="$1"
+runner_os="$2"
+editor_dir=~/.godot
+
+if ! mkdir -p ${editor_dir}; then
+    echo "Error: Failed to create directory at ${editor_dir}."
+    exit 1
+fi
+
+case ${runner_os} in
+"Linux")
+    platform="linux_x86_64"
+    ;;
+"macOS")
+    platform="macos.universal"
+    ;;
+*)
+    echo "Error: Unsupported platform ${runner_os}."
+    exit 1
+    ;;
+esac
+
+file_name=Godot_v${version}-stable_mono_${platform}
+url=https://github.com/godotengine/godot/releases/download/${version}-stable/${file_name}.zip
+downloaded_file=${editor_dir}/${file_name}.zip
+
+if ! curl -L -o "${downloaded_file}" "${url}"; then
+    echo "Error: Download failed for ${url}."
+    exit 1
+fi
+
+if [ ! -f "${downloaded_file}" ]; then
+    echo "Error: Downloaded file ${downloaded_file} not found!"
+    exit 1
+fi
+
+if ! unzip -o "${downloaded_file}" -d ${editor_dir}; then
+    echo "Error: Extraction failed for ${downloaded_file}."
+    exit 1
+fi
+
+rm "${downloaded_file}"
+
+case "${runner_os}" in
+"Linux")
+    mv "${editor_dir}/Godot_v${version}-stable_mono_linux_x86_64" "${editor_dir}/Godot_v${version}"
+    ;;
+"macOS")
+    mv "${editor_dir}/Godot_mono.app" "${editor_dir}/Godot_v${version}.app"
+    ;;
+*)
+    echo "Error: Unsupported platform ${runner_os}."
+    exit 1
+    ;;
+esac
---
+++ .github/actions/setup-godot/scripts/download_templates.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <version> <templates_dir> <platforms>"
+    exit 1
+fi
+
+version="$1"
+templates_dir="$2"
+platforms="$3"
+
+platform_patterns=$(echo "$platforms" | tr ',' '\n' | sed 's/^[[:space:]]*\(.*\)[[:space:]]*$/\1/' | awk '{print "templates/" tolower($0) "*"}' | paste -sd ' ' -)
+
+if ! mkdir -p "${templates_dir}"; then
+    echo "Error: Failed to create directory at ${templates_dir}."
+    exit 1
+fi
+
+file_name=${version}.stable.mono
+url=https://github.com/godotengine/godot/releases/download/${version}-stable/Godot_v${version}-stable_mono_export_templates.tpz
+downloaded_file=${templates_dir}/${file_name}.tpz
+
+if ! curl -L -o "${downloaded_file}" "${url}"; then
+    echo "Error: Download failed for ${url}."
+    exit 1
+fi
+
+if [ ! -f "${downloaded_file}" ]; then
+    echo "Error: Downloaded file ${downloaded_file} not found!"
+    exit 1
+fi
+
+# shellcheck disable=SC2086
+if ! unzip -o "${downloaded_file}" "templates/version.txt" ${platform_patterns} -d "${templates_dir}"; then
+    echo "Error: Extraction failed for ${downloaded_file} with patterns ${platform_patterns}."
+    exit 1
+fi
+
+rm "${downloaded_file}"
+mv "${templates_dir}/templates" "${templates_dir}/${file_name}"
