67bb16e50a474ed3dcb30995aa5708691cb086e2
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-11-21 00:04:23 +0300

    feat: add install_android_template.sh

---
+++ .github/actions/build-godot/scripts/install_android_template.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <project_dir> <templates_dir>"
+    exit 1
+fi
+
+project_dir="$1"
+templates_dir="$2"
+
+android_source="${templates_dir}/android_source.zip"
+android_dir="${project_dir}/android"
+build_dir="${android_dir}/build"
+
+if ! mkdir -p "${build_dir}"; then
+    echo "Error: Failed to create directory at ${build_dir}."
+    exit 1
+fi
+
+if ! unzip -o "${android_source}" -d "${build_dir}"; then
+    echo "Error: Extraction failed for ${android_source}."
+    exit 1
+fi
+
+if ! cp "${templates_dir}/version.txt" "${android_dir}/.build_version"; then
+    echo "Error: Failed to copy version file to ${android_dir}/.build_version."
+    exit 1
+fi
+
+if ! touch "${build_dir}/.gdignore"; then
+    echo "Error: Failed to create .gdignore file in ${build_dir}."
+    exit 1
+fi
