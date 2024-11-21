fa9bd59fc39ab3f82a2fcb7f1df665efcd1f4722
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-11-21 16:41:53 +0300

    feat: add godot command

--- .github/actions/setup-godot/scripts/install_editor.sh
+++ .github/actions/setup-godot/scripts/install_editor.sh
@@ -10,10 +10,12 @@ runner_os="$2"
 
 case ${runner_os} in
 Linux)
-    executable_file=~/.godot/Godot_v${version}/Godot_v${version}-stable_mono_linux.x86_64
+    godot_executable=~/.godot/Godot_v${version}/Godot_v${version}-stable_mono_linux.x86_64
+    godot_sharp=~/.godot/Godot_v${version}/GodotSharp
     ;;
 macOS)
-    executable_file=~/.godot/Godot_v${version}.app/Contents/MacOS/Godot
+    godot_executable=~/.godot/Godot_v${version}.app/Contents/MacOS/Godot
+    godot_sharp=~/.godot/Godot_v${version}/Resources/GodotSharp
     ;;
 *)
     echo "Error: Unsupported platform ${runner_os}."
@@ -21,9 +23,25 @@ macOS)
     ;;
 esac
 
-if [ ! -f "${executable_file}" ]; then
-    echo "Error: File ${executable_file} not found!"
+if [ ! -f "${godot_executable}" ]; then
+    echo "Error: File ${godot_executable} not found!"
     exit 1
 fi
 
-echo "${executable_file}"
+if [ ! -d "${godot_sharp}" ]; then
+    echo "Error: Directory ${godot_sharp} not found!"
+    exit 1
+fi
+
+if [ -L "/usr/local/bin/godot" ]; then
+    echo "Removing existing Godot symlink..."
+    sudo rm -f /usr/local/bin/godot
+fi
+
+if [ -L "/usr/local/bin/GodotSharp" ]; then
+    echo "Removing existing GodotSharp symlink..."
+    sudo rm -f /usr/local/bin/GodotSharp
+fi
+
+sudo ln -s "${godot_executable}" /usr/local/bin/godot
+sudo ln -s "${godot_sharp}" /usr/local/bin/GodotSharp
