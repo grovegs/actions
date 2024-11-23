f7f2485831955ea7d70fed4f94bbbaf3c13553cb
Onur Tanrıkulu <onurtnrkl@users.noreply.github.com>
2024-11-23 18:44:44 +0300

    feat: build-godot (#13)
    * chore: rename godot application sandbox
    
    * feat: add install_android_template.sh
    
    * feat: add get_templates_dir.sh
    
    * feat: add update_export_presets.sh
    
    * chore: rename template-platforms
    
    * feat: add get_android_options.sh
    
    * feat: add get_ios_options.sh
    
    * wip: build-godot action
    
    * chore: update gitignore
    
    * test: add export presets to sandbox
    
    * feat: add artifact upload
    
    * Update setup-godot-tests.yml
    
    * Create build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * fix: godot sandbox path
    
    * fix: update variable names in tests
    
    * Update tests.yml
    
    * Update setup-godot-tests.yml
    
    * chore: rename get version with get godot version
    
    * fix: android template source path
    
    * fix: fix template dir path
    
    * Update action.yml
    
    * Update build-godot-tests.yml
    
    * Update action.yml
    
    * fix: update artifacts path
    
    * Revert "fix: update artifacts path"
    
    This reverts commit b6f2587ee14f64fc4ed1d33bcecad686340f702c.
    
    * Reapply "fix: update artifacts path"
    
    This reverts commit 63ccd77e7c50754d94b08822bc914187e1f870c0.
    
    * Revert "Reapply "fix: update artifacts path""
    
    This reverts commit 0dead71af923dd17fc5e812ad488fe29bb9ce149.
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update build-godot-tests.yml
    
    * fix: update keystore access
    
    * Update action.yml
    
    * test: update godot sandbox
    
    * Update action.yml
    
    * fix: add keystore inputs
    
    * Update action.yml
    
    * Update action.yml
    
    * test: add debug steps
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update action.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update action.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update action.yml
    
    * Update project.godot
    
    * Update action.yml
    
    * fix: fix artifacts path
    
    * refactor: move export android to export project
    
    * feat: add android format
    
    * fix: syntax error
    
    * feat: replace artifacts path
    
    * Update action.yml
    
    * Update action.yml
    
    * Update export_android_project.sh
    
    * Update export_android_project.sh
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update export_android_project.sh
    
    * Update action.yml
    
    * Update action.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * Update build-godot-tests.yml
    
    * feat: add export configuration
    
    * feat: add validate platform inputs
    
    * fix: update validate platform inputs

---
+++ .github/actions/build-godot/scripts/get_templates_dir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <runner_os>"
+    exit 1
+fi
+
+runner_os="$1"
+
+case ${runner_os} in
+"Linux")
+    templates_dir=~/.local/share/godot/export_templates
+    ;;
+"macOS")
+    templates_dir=~/Library/Application\ Support/Godot/export_templates
+    ;;
+*)
+    echo "Error: Unsupported platform ${runner_os}."
+    exit 1
+    ;;
+esac
+
+echo "${templates_dir}"
---
+++ .github/actions/build-godot/scripts/install_android_template.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <project_dir> <templates_dir> <godot_version>"
+    exit 1
+fi
+
+project_dir="$1"
+templates_dir="$2"
+godot_version="$3"
+
+template_dir=${templates_dir}/${godot_version}.stable.mono
+android_source="${template_dir}/android_source.zip"
+android_dir="${project_dir}/android"
+build_dir="${android_dir}/build"
+
+if ! mkdir -p "${build_dir}"; then
+    echo "Error: Failed to create directory at ${build_dir}."
+    exit 1
+fi
+
+if ! unzip -o "${android_source}" -d "${build_dir}"; then
+    echo "Error: Extraction failed for ${android_source}."
+    exit 1
+fi
+
+if ! cp "${template_dir}/version.txt" "${android_dir}/.build_version"; then
+    echo "Error: Failed to copy version file to ${android_dir}/.build_version."
+    exit 1
+fi
+
+if ! touch "${build_dir}/.gdignore"; then
+    echo "Error: Failed to create .gdignore file in ${build_dir}."
+    exit 1
+fi
