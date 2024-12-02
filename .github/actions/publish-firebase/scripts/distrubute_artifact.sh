016738b5c23e1937ecf44e0d2476e4cb4fce4b9b
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-12-02 19:47:22 +0300

    refactor: update publish firebase inputs

--- .github/actions/publish-firebase/scripts/distrubute_artifact.sh
+++ .github/actions/publish-firebase/scripts/distrubute_artifact.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <artifact_name> <app_id> <credentials> [release_notes] [tester_groups]"
+    echo "Usage: $0 <file> <app_id> <credentials> [release_notes] [tester_groups]"
     exit 1
 fi
 
-artifact_name="$1"
+file="$1"
 app_id="$2"
 credentials="$3"
 release_notes="${4:-}"
 tester_groups="${5:-}"
 
-artifacts_dir=~/.artifacts
 firebase_dir=~/.firebase
-artifact=$(find "${artifacts_dir}" -type f -name "${artifact_name}*" | head -n 1)
 credentials_file=${firebase_dir}/credentials_file.json
 
-if [ -z "${artifact}" ]; then
-    echo "Error: No artifact found matching '${artifact_name}' in '${artifacts_dir}'"
+if [ -z "${file}" ]; then
+    echo "Error: No file found matching '${file}'."
     exit 1
 fi
 
@@ -32,7 +30,7 @@ if ! echo -n "${credentials}" | base64 -d >${credentials_file}; then
 fi
 
 export GOOGLE_APPLICATION_CREDENTIALS=${credentials_file}
-firebase_command="firebase appdistribution:distribute \"${artifact}\" --app \"${app_id}\""
+firebase_command="firebase appdistribution:distribute \"${file}\" --app \"${app_id}\""
 
 if [ -n "${release_notes}" ]; then
     firebase_command+=" --release-notes \"${release_notes}\""
