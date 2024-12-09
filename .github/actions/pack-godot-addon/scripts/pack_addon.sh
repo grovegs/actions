272bfa42d113addcc3776a645d77a4cfb69c4718
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-12-09 19:16:54 +0300

    Update pack_addon.sh

--- .github/actions/pack-godot-addon/scripts/pack_addon.sh
+++ .github/actions/pack-godot-addon/scripts/pack_addon.sh
@@ -9,27 +9,26 @@ addon="$1"
 version="$2"
 filename="$3"
 
-addons_dir=~/.godot/addons
-
 if [[ ! -d "${addon}" ]]; then
     echo "Error: Addon directory '${addon}' does not exist."
     exit 1
 fi
 
-addon_name=$(basename "${addon}")
-addon_path="${addons_dir}/${addon_name}"
+addons_dir=~/.godot/addons
+temp=${addons_dir}/${filename}
+trap '[[ -d "${temp}" ]] && rm -rf "${temp}"' EXIT
 
-if ! mkdir -p "${addon_path}"; then
-    echo "Error: Failed to create addon directory '${addon_path}'."
+if ! mkdir -p "${temp}"; then
+    echo "Error: Failed to create addons directory '${temp}'."
     exit 1
 fi
 
-if ! cp -r "${addon}" "${addon_path}"; then
-    echo "Error: Failed to copy addon '${addon}' to '${addon_path}'."
+if ! cp -r "${addon}" "${temp}"; then
+    echo "Error: Failed to copy addon '${addon}' to '${temp}'."
     exit 1
 fi
 
-addon_dir="${addon_path}/$(basename "${addon}")"
+addon_dir="${temp}/$(basename "${addon}")"
 
 if [[ -f "README.md" ]] && ! cp README.md "${addon_dir}"; then
     echo "Error: Failed to copy README.md to '${addon_dir}'."
@@ -41,17 +40,17 @@ if [[ -f "LICENSE" ]] && ! cp LICENSE "${addon_dir}"; then
     exit 1
 fi
 
-if ! sed -i "s/version=\"[^\"]*\"/version=\"${version}\"/" "${addon_dir}/plugin.cfg"; then
+if ! sed -i '' 's/version="[^"]*"/version="'${version}'"/' "$addon_dir/plugin.cfg"; then
     echo "Error: Failed to update version in 'plugin.cfg'."
     exit 1
 fi
 
 cd "${addons_dir}" || exit 1
-zip_file="${filename}.zip"
-file="${addons_dir}/${zip_file}"
+zip_name="${filename}.zip"
+file="${addons_dir}/${zip_name}"
 
-if ! zip --recurse-paths --quiet "${zip_file}" "${addon_name}/*"; then
-    echo "Error: Failed to create zip archive '${zip_file}'."
+if ! zip --recurse-paths --quiet "${zip_name}" "${filename}"; then
+    echo "Error: Failed to create zip archive '${zip_name}'."
     exit 1
 fi
 
