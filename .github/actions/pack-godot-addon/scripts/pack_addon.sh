59133dea5899b5d0efb3f423099a846a76caf08a
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-12-09 11:57:59 +0300

    refactor: update pack godot addon

---
+++ .github/actions/pack-godot-addon/scripts/pack_addon.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <addon> <version> <filename>"
+    exit 1
+fi
+
+addon="$1"
+version="$2"
+filename="$3"
+
+addons_dir=~/.godot/addons
+
+if [[ ! -d "${addon}" ]]; then
+    echo "Error: Addon directory '${addon}' does not exist."
+    exit 1
+fi
+
+addon_name=$(basename "${addon}")
+addon_path="${addons_dir}/${addon_name}"
+
+if ! mkdir -p "${addon_path}"; then
+    echo "Error: Failed to create addon directory '${addon_path}'."
+    exit 1
+fi
+
+if ! cp -r "${addon}" "${addon_path}"; then
+    echo "Error: Failed to copy addon '${addon}' to '${addon_path}'."
+    exit 1
+fi
+
+addon_dir="${addon_path}/$(basename "${addon}")"
+
+if [[ ! -f "README.md" ]]; then
+    echo "Warning: README.md not found."
+else
+    if ! cp README.md "${addon_dir}"; then
+        echo "Error: Failed to copy README.md to '${addon_dir}'."
+        exit 1
+    fi
+fi
+
+if [[ ! -f "LICENSE" ]]; then
+    echo "Warning: LICENSE not found."
+else
+    if ! cp LICENSE "${addon_dir}"; then
+        echo "Error: Failed to copy LICENSE to '${addon_dir}'."
+        exit 1
+    fi
+fi
+
+if ! sed -i "s/version=\"[^\"]*\"/version=\"${version}\"/" "${addon_dir}/plugin.cfg"; then
+    echo "Error: Failed to update version in 'plugin.cfg'."
+    exit 1
+fi
+
+cd "${addons_dir}" || exit
+zip_file="${filename}.zip"
+
+if ! zip -r "${zip_file}" "${addon_name}"; then
+    echo "Error: Failed to create zip archive '${zip_file}'."
+    exit 1
+fi
+
+echo "${addons_dir}/${zip_file}"
