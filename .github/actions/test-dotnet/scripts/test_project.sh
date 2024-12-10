33446d4d741f0550030c65c13f83316bc2f712a0
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-12-10 13:29:51 +0300

    Create test_project.sh

---
+++ .github/actions/test-dotnet/scripts/test_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <project> <configuration>"
+    exit 1
+fi
+
+project="$1"
+configuration="$2"
+
+if [[ ! -d "${project}" ]]; then
+    echo "Error: Project directory '${project}' does not exist."
+    exit 1
+fi
+
+project_file="${project}/$(basename "${project}").csproj"
+
+if [[ ! -f "${project_file}" ]]; then
+    echo "Error: Project file '${project_file}' does not exist."
+    exit 1
+fi
+
+dotnet test --nologo --configuration "${configuration}" "${project_file}"
