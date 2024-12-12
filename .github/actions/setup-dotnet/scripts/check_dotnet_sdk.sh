c91e83125d15db8f3fdda2be17b764cc77a9f7ee
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-12-12 11:07:37 +0300

    feat: add dotnet setup caching

---
+++ .github/actions/setup-dotnet/scripts/check_dotnet_sdk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <sdk_version>"
+    exit 1
+fi
+
+sdk_version="$1"
+
+if [ -z "${sdk_version}" ]; then
+    echo "Error: SDK version not provided."
+    exit 1
+fi
+
+if dotnet --list-sdks | grep -q "^${sdk_version}"; then
+    echo "true"
+else
+    echo "false"
+fi
