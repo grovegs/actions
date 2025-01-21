9abbaeb390f02f517761da306ea3f3b5348827b3
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-01-21 11:23:13 +0300

    Update get_godot_info.sh

--- .github/actions/setup-godot/scripts/get_godot_info.sh
+++ .github/actions/setup-godot/scripts/get_godot_info.sh
@@ -7,31 +7,29 @@ fi
 
 global_json_file="$1"
 
-sdk_line=$(grep '"Godot\.NET\.Sdk":' "${global_json_file}")
-full_version=${sdk_line#*: \"}
-full_version=${full_version%\"*}
+full_version=$(sed -n 's/.*"Godot\.NET\.Sdk": "\([^"]*\)".*/\1/p' "${global_json_file}")
 
 if [ -z "${full_version}" ]; then
     echo "::error::Version not found in ${global_json_file}."
     exit 1
 fi
 
-version=${full_version%-*}
-stage=${full_version#*-}
-
-if [ "${stage}" == "${full_version}" ]; then
-    stage="stable"
-else
+if [[ "${full_version}" == *"-"* ]]; then
+    version=${full_version%%-*}
+    stage=${full_version#*-}
     stage=${stage//./}
+else
+    version=${full_version}
+    stage="stable"
 fi
 
 IFS='.' read -r major minor patch <<<"${version}"
 
-if [ "${patch}" -eq 0 ]; then
-    version=${major}.${minor}
+if [ "${patch}" = "0" ]; then
+    version="${major}.${minor}"
 fi
 
 echo "::notice::Godot version is ${version}"
 echo "::notice::Godot stage is ${stage}"
-echo version="$version" >>"$GITHUB_OUTPUT"
-echo stage="$stage" >>"$GITHUB_OUTPUT"
+echo "version=${version}" >>"$GITHUB_OUTPUT"
+echo "stage=${stage}" >>"$GITHUB_OUTPUT"
