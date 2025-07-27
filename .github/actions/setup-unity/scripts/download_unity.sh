6ecb4bf67ead5ab45cb3d8680cd9a098b1c6eb81
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-07-27 16:54:44 +0300

    Update download_unity.sh

--- .github/actions/setup-unity/scripts/download_unity.sh
+++ .github/actions/setup-unity/scripts/download_unity.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-if [ "$#" -ne 4 ]; then
-    echo "::error::Usage: $0 <version> <changeset> <modules> <download_dir>"
-    exit 1
-fi
+if [ "$#" -ne 4 ]; then echo "::error::Usage: $0 <version> <changeset> <modules> <download_dir>"; exit 1; fi
 version="$1"
 changeset="$2"
 modules="$3"
@@ -14,32 +11,21 @@ download_file() {
     if ! curl --fail -L -o "${file_path}" "${url}"; then echo "::error::Failed to download from ${url} (server returned an error)"; exit 1; fi
 }
 base_url="https://download.unity3d.com/download_unity/${changeset}"
-
 if [[ "$RUNNER_OS" == "macOS" ]]; then
     editor_url="${base_url}/MacEditorInstallerArm64/Unity-${version}.pkg"
 elif [[ "$RUNNER_OS" == "Linux" ]]; then
     editor_url="${base_url}/LinuxEditorInstaller/Unity.tar.xz"
 fi
 download_file "${editor_url}" "${download_dir}/$(basename "${editor_url}")"
-
-if [ -n "${modules}" ]; then
-    if [[ "$RUNNER_OS" == "macOS" ]]; then
-        module_path_segment="MacEditorTargetInstaller"
-    elif [[ "$RUNNER_OS" == "Linux" ]]; then
-        module_path_segment="TargetSupportInstaller"
-    fi
+if [ -n "${modules}" ] && [[ "$RUNNER_OS" == "macOS" ]]; then
+    module_path_segment="MacEditorTargetInstaller"
     IFS=',' read -ra MODULE_ARRAY <<< "${modules}"
     for module in "${MODULE_ARRAY[@]}"; do
         module_trimmed=$(echo "${module}" | xargs); module_url=""
         case "${module_trimmed}" in
-            "android")
-                if [[ "$RUNNER_OS" == "macOS" ]]; then module_url="${base_url}/${module_path_segment}/UnitySetup-Android-Support-for-Editor-${version}.pkg";
-                elif [[ "$RUNNER_OS" == "Linux" ]]; then module_url="${base_url}/${module_path_segment}/Android.tar.xz"; fi;;
-            "ios")
-                if [[ "$RUNNER_OS" == "macOS" ]]; then module_url="${base_url}/${module_path_segment}/UnitySetup-iOS-Support-for-Editor-${version}.pkg"; fi;;
-            "webgl")
-                if [[ "$RUNNER_OS" == "macOS" ]]; then module_url="${base_url}/${module_path_segment}/UnitySetup-WebGL-Support-for-Editor-${version}.pkg";
-                elif [[ "$RUNNER_OS" == "Linux" ]]; then module_url="${base_url}/${module_path_segment}/WebGL.tar.xz"; fi;;
+            "android") module_url="${base_url}/${module_path_segment}/UnitySetup-Android-Support-for-Editor-${version}.pkg";;
+            "ios") module_url="${base_url}/${module_path_segment}/UnitySetup-iOS-Support-for-Editor-${version}.pkg";;
+            "webgl") module_url="${base_url}/${module_path_segment}/UnitySetup-WebGL-Support-for-Editor-${version}.pkg";;
             *) echo "::warning::Skipping unknown module: ${module_trimmed}";;
         esac
         if [ -n "${module_url}" ]; then download_file "${module_url}" "${download_dir}/$(basename "${module_url}")"; fi
